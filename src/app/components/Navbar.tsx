a2fe4b4eae389e70041ae457f0e15001ca319f16
Alek30k <alemank23k@gmail.com>
2024-05-29 13:58:41 -0300

    last commit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
       {/* MOBILE */}
-      <div className="h-full flex items-center justify-between">
+      <div className="h-full flex items-center justify-between md:hidden">
         <Link href="/">
           <div className="text-2xl tracking-wide">ArCdev</div>
         </Link>
@@ -18,10 +18,10 @@ const Navbar = () => {
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
         {/* LEFT */}
         <div className="w-1/3">
-          <link href="/">
+          <Link href="/" className="flex items-center gap-3">
             <Image src={"/logo.png"} width={24} height={24} alt="ArCdev-Logo" />
             <div className="text-2xl tracking-wide">ArCdev</div>
-          </link>
+          </Link>
         </div>
         {/* RIGHT */}
         <div className="w-2/3 flex items-center justify-between gap-8">
