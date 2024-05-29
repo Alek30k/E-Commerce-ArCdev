f3fac35a4483bdc9be73682f7bfccf298b8b4513
Alek30k <alemank23k@gmail.com>
2024-05-29 14:05:09 -0300

    last commit

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+
 const SearchBar = () => {
-  return <div className="">SearchBar</div>;
+  return (
+    <form className="flex items-center justify-between gap-4 bg-gray-100 p-2 rounded-md">
+      <input type="text" placeholder="Search" />
+      <button>
+        <Image src="/search.png" alt="search" width={16} height={16} />
+      </button>
+    </form>
+  );
 };
 
 export default SearchBar;
