fcbd703f80389f5768a94efb3bb97fd40381e587
Alek30k <alemank23k@gmail.com>
2024-05-28 23:19:53 -0300

    last commit

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -18,7 +18,7 @@ const Menu = () => {
         onClick={() => setOpen(!open)}
       />
       {open && (
-        <div className="absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80px)]">
+        <div className="absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col justify-center items-center gap-8 text-xl z-10">
           <Link href="/">Homepage</Link>
           <Link href="/">Shop</Link>
           <Link href="/">Deals</Link>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,28 @@
 import Link from "next/link";
 import Menu from "./Menu";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
+      {/* MOBILE */}
       <div className="h-full flex items-center justify-between">
         <Link href="/">
           <div className="text-2xl tracking-wide">ArCdev</div>
         </Link>
         <Menu />
+      </div>
+      {/* BIGGER SCREENS */}
+      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+        {/* LEFT */}
+        <div className="w-1/3">
+          <link href="/">
+            <Image src={"/logo.png"} width={24} height={24} alt="ArCdev-Logo" />
+            <div className="text-2xl tracking-wide">ArCdev</div>
+          </link>
+        </div>
+        {/* RIGHT */}
+        <div className="w-2/3"></div>
       </div>
     </div>
   );
