deec3f5d5d4713a458bf87561584814a8bc6284c
Alek30k <alemank23k@gmail.com>
2024-05-28 23:05:39 -0300

    last commit

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,11 +1,34 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
 
-  return <div className="">Menu</div>;
+  return (
+    <div className="">
+      <Image
+        src="/menu.png"
+        alt="Menu"
+        width={28}
+        height={28}
+        className="cursor-pointer"
+        onClick={() => setOpen(!open)}
+      />
+      {open && (
+        <div className="absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80px)]">
+          <Link href="/">Homepage</Link>
+          <Link href="/">Shop</Link>
+          <Link href="/">Deals</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Logout</Link>
+          <Link href="/">Cart(1)</Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Menu;
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,9 +3,13 @@ import Menu from "./Menu";
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl-32 2xl:px-64">
-      <Link href="/">ArCdev</Link>
-      <Menu />
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
+      <div className="h-full flex items-center justify-between">
+        <Link href="/">
+          <div className="text-2xl tracking-wide">ArCdev</div>
+        </Link>
+        <Menu />
+      </div>
     </div>
   );
 };
