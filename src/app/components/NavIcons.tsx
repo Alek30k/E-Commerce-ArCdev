320d1612ac3cdc0e86499f8ae86a4e3c5bbe8948
Alek30k <alemank23k@gmail.com>
2024-05-30 10:26:40 -0300

    last commit

--- src/app/components/NavIcons.tsx
+++ src/app/components/NavIcons.tsx
@@ -31,7 +31,7 @@ const NavIcons = () => {
         onClick={handleProfile}
       />
       {isProfileOpen && (
-        <div className="">
+        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
           <Link href="/">Profile</Link>
           <div className="mt-2 cursor-pointer">Logout</div>
         </div>
