2498844e945c230bba3f4261bf1b2245d6b30f84
Alek30k <alemank23k@gmail.com>
2024-06-01 15:07:27 -0300

    last commit

---
+++ src/app/components/CategoryList.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const CategoryList = () => {
+  return (
+    <div className="px-4 overflow-scroll">
+      <div className="flex gap-4 md:gap-8">
+        <Link href="/"></Link>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
