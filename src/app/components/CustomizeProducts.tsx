25fbf2e6a1d1a5eb84e85e9523011ceff93dfa81
Alek30k <alemank23k@gmail.com>
2024-06-02 22:54:14 -0300

    last commit

--- src/app/components/CustomizeProducts.tsx
+++ src/app/components/CustomizeProducts.tsx
@@ -1,5 +1,9 @@
 const CustomizeProducts = () => {
-  return <div className="">CustomizeProducts</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      <h4 className="">Choose a Size</h4>
+    </div>
+  );
 };
 
 export default CustomizeProducts;
