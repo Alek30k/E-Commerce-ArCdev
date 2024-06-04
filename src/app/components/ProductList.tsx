8ebce09f6f99d43036326c2dceb88508daad9b49
Alek30k <alemank23k@gmail.com>
2024-06-04 16:14:09 -0300

    last commit

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -2,21 +2,24 @@ import { wixClientServer } from "@/lib/wixClientServer";
 import { products } from "@wix/stores";
 import Image from "next/image";
 import Link from "next/link";
+// import DOMPurify from "isomorphic-dompurify";
+// import Pagination from "./Pagination";
 
-const PRODUCT_PER_PAGE = 20;
+const PRODUCT_PER_PAGE = 8;
 
 const ProductList = async ({
   categoryId,
   limit,
-}: {
+}: // searchParams,
+{
   categoryId: string;
   limit?: number;
+  // searchParams?: any;
 }) => {
   const wixClient = await wixClientServer();
 
   const res = await wixClient.products
     .queryProducts()
-    .eq("collectionIds", categoryId)
     .limit(limit || PRODUCT_PER_PAGE)
     .find();
 
@@ -26,36 +29,56 @@ const ProductList = async ({
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
       {res.items.map((product: products.Product) => (
         <Link
-          href="/test"
+          href={"/" + product.slug}
           className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
           key={product._id}
         >
           <div className="relative w-full h-80">
             <Image
-              src="https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&w=600"
+              src={product.media?.mainMedia?.image?.url || "/product.png"}
               alt=""
               fill
               sizes="25vw"
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease duration-500"
+              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
             />
-            <Image
-              src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
-              alt=""
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md"
-            />
+            {product.media?.items && (
+              <Image
+                src={product.media?.items[1]?.image?.url || "/product.png"}
+                alt=""
+                fill
+                sizes="25vw"
+                className="absolute object-cover rounded-md"
+              />
+            )}
           </div>
           <div className="flex justify-between">
-            <span className="font-medium">Product Name</span>
-            <span className="font-semibold">$49</span>
+            <span className="font-medium">{product.name}</span>
+            <span className="font-semibold">${product.price?.price}</span>
           </div>
-          <div className="text-sm text-gray-500">My description</div>
-          <button className="rounded-2xl ring-1 w-max ring-ale text-ale py-2 px-4 text-xs hover:bg-ale hover:text-white">
+          {product.additionalInfoSections && (
+            <div
+              className="text-sm text-gray-500"
+              // dangerouslySetInnerHTML={{
+              //   __html: DOMPurify.sanitize(
+              //     product.additionalInfoSections.find(
+              //       (section: any) => section.title === "shortDesc"
+              //     )?.description || ""
+              //   ),
+              // }}
+            ></div>
+          )}
+          <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
             Add to Cart
           </button>
         </Link>
       ))}
+      {/* {searchParams?.cat || searchParams?.name ? (
+        <Pagination
+          currentPage={res.currentPage || 0}
+          hasPrev={res.hasPrev()}
+          hasNext={res.hasNext()}
+        />
+      ) : null} */}
     </div>
   );
 };
--- src/lib/wixClientServer.ts
+++ src/lib/wixClientServer.ts
@@ -1,22 +1,23 @@
 import { OAuthStrategy, createClient } from "@wix/sdk";
-import { products, collections } from "@wix/stores";
+import { collections, products } from "@wix/stores";
+// import { orders } from "@wix/ecom";
 import { cookies } from "next/headers";
+// import { members } from '@wix/members';
 
 export const wixClientServer = async () => {
   let refreshToken;
 
   try {
     const cookieStore = cookies();
-
     refreshToken = JSON.parse(cookieStore.get("refreshToken")?.value || "{}");
-  } catch (error) {}
+  } catch (e) {}
 
   const wixClient = createClient({
     modules: {
       products,
       collections,
-      // currentCart,
-      // redirects,
+      //   orders,
+      //   members,
     },
     auth: OAuthStrategy({
       clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
