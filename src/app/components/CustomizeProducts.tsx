24be095cc959d63c4996e3ceedf28d45955a7af8
Alek30k <alemank23k@gmail.com>
2024-06-05 15:52:32 -0300

    last commit

--- src/app/components/CustomizeProducts.tsx
+++ src/app/components/CustomizeProducts.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { products } from "@wix/stores";
+import { useState } from "react";
 
 const CustomizeProducts = ({
   productId,
@@ -11,11 +12,35 @@ const CustomizeProducts = ({
   variants: products.Variant[];
   productOptions: products.ProductOption[];
 }) => {
+  const [selectedOptions, setSelectedOptions] = useState<{
+    [key: string]: string;
+  }>({});
+
+  const handleOptionSelect = (optionType: string, choice: string) => {
+    setSelectedOptions((prev) => ({ ...prev, [optionType]: choice }));
+  };
+
+  const isVariantInStock = (choices: { [key: string]: string }) => {
+    return variants.some((variant) => {
+      const variantChoices = variant.choices;
+      if (!variantChoices) return false;
+    });
+  };
+
   return (
     <div className="flex flex-col gap-6">
       {productOptions.map((option) => (
         <div className="flex flex-col gap-4" key={option.name}>
           <h4 className="font-medium">Choose a {option.name}</h4>
+          {option.choices?.map((choice) => (
+            <div className="" key={choice.value}>
+              {choice.description}
+            </div>
+          ))}
+        </div>
+      ))}
+      {/* COLOR */}
+      {/* <h4 className="font-medium">Choose a {option.name}</h4>
           <ul className="flex items-center gap-3">
             <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500">
               <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
@@ -24,9 +49,8 @@ const CustomizeProducts = ({
             <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-not-allowed relative bg-green-500">
               <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
             </li>
-          </ul>
-        </div>
-      ))}
+          </ul> */}
+      {/* OTHERS */}
       {/* <h4 className="font-medium">Choose a size</h4>
       <ul className="flex items-center gap-3">
         <li className="ring-1 ring-ale text-ale rounded-md py-1 px-4 text-sm cursor-pointer">
