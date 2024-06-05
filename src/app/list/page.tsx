c49e6255e7e3875848d62bc4ac40d294a410a772
Alek30k <alemank23k@gmail.com>
2024-06-04 22:53:20 -0300

    last commit

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -2,11 +2,15 @@ import Image from "next/image";
 import Filter from "../components/Filter";
 import ProductList from "../components/ProductList";
 import { wixClientServer } from "@/lib/wixClientServer";
+import { Suspense } from "react";
+import Skeleton from "../components/Skeleton";
 
 const ListPage = async ({ searchParams }: { searchParams: any }) => {
   const wixClient = await wixClientServer();
 
-  const response = await wixClient.collections.getCollectionBySlug(slug);
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
 
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
@@ -32,10 +36,14 @@ const ListPage = async ({ searchParams }: { searchParams: any }) => {
       <Filter />
       {/* PRODUCTS */}
       <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
-      <ProductList
-        categoryId={process.env.FEATURED_PRODUCTS_FEATURED_CATEGORY_ID!}
-        limit={4}
-      />
+      <Suspense fallback={<Skeleton />}>
+        <ProductList
+          categoryId={
+            cat.collection?._id || "00000000-000000-000000-000000000001"
+          }
+          searchParams={searchParams}
+        />
+      </Suspense>
     </div>
   );
 };
