a225f07f3a85aabac2910db5578a89ed76208355
Alek30k <alemank23k@gmail.com>
2024-06-04 23:32:45 -0300

    last commit

--- src/app/components/ProductImages.tsx
+++ src/app/components/ProductImages.tsx
@@ -3,33 +3,33 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const images = [
-  {
-    id: 1,
-    url: "https://images.pexels.com/photos/25047788/pexels-photo-25047788/free-photo-of-madera-paisaje-agua-verano.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
-  },
-  {
-    id: 2,
-    url: "https://images.pexels.com/photos/23947602/pexels-photo-23947602/free-photo-of-amor-gente-mujer-picnic.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
-  },
-  {
-    id: 3,
-    url: "https://images.pexels.com/photos/22707220/pexels-photo-22707220/free-photo-of-moda-hombre-amor-gente.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
-  },
-  {
-    id: 4,
-    url: "https://images.pexels.com/photos/21821538/pexels-photo-21821538/free-photo-of-ligero-mar-paisaje-montanas.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
-  },
-];
+// const images = [
+//   {
+//     id: 1,
+//     url: "https://images.pexels.com/photos/25047788/pexels-photo-25047788/free-photo-of-madera-paisaje-agua-verano.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+//   },
+//   {
+//     id: 2,
+//     url: "https://images.pexels.com/photos/23947602/pexels-photo-23947602/free-photo-of-amor-gente-mujer-picnic.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+//   },
+//   {
+//     id: 3,
+//     url: "https://images.pexels.com/photos/22707220/pexels-photo-22707220/free-photo-of-moda-hombre-amor-gente.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+//   },
+//   {
+//     id: 4,
+//     url: "https://images.pexels.com/photos/21821538/pexels-photo-21821538/free-photo-of-ligero-mar-paisaje-montanas.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+//   },
+// ];
 
-const ProductImages = () => {
+const ProductImages = ({ items }: { items: any }) => {
   const [index, setIndex] = useState(0);
 
   return (
     <div className="">
       <div className="h-[500px] relative">
         <Image
-          src={images[index].url}
+          src={items[index].image?.url}
           alt=""
           fill
           sizes="50vw"
@@ -37,14 +37,14 @@ const ProductImages = () => {
         />
       </div>
       <div className="flex justify-between gap-4 mt-8">
-        {images.map((img, i) => (
+        {items.map((item: any, i: number) => (
           <div
             className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
-            key={img.id}
+            key={item._id}
             onClick={() => setIndex(i)}
           >
             <Image
-              src={img.url}
+              src={item.image?.url}
               alt=""
               fill
               sizes="30vw"
