94538723ddea9175651769cfc68cfe568c52f45c
Alek30k <alemank23k@gmail.com>
2024-06-05 21:30:48 -0300

    last commit

--- src/app/components/CustomizeProducts.tsx
+++ src/app/components/CustomizeProducts.tsx
@@ -36,14 +36,11 @@ const CustomizeProducts = ({
     });
   };
 
-  console.log(selectedOptions);
-
   return (
     <div className="flex flex-col gap-6">
       {productOptions.map((option) => (
         <div className="flex flex-col gap-4" key={option.name}>
           <h4 className="font-medium">Choose a {option.name}</h4>
-
           <ul className="flex items-center gap-3">
             {option.choices?.map((choice) => {
               const disabled = !isVariantInStock({
@@ -54,9 +51,9 @@ const CustomizeProducts = ({
               const selected =
                 selectedOptions[option.name!] === choice.description;
 
-              // const clickHandler = disabled
-              //   ? undefined
-              //   : () => handleOptionSelect(option.name!, choice.description!);
+              const clickHandler = disabled
+                ? undefined
+                : () => handleOptionSelect(option.name!, choice.description!);
 
               return option.name === "Color" ? (
                 <li
@@ -65,7 +62,7 @@ const CustomizeProducts = ({
                     backgroundColor: choice.value,
                     cursor: disabled ? "not-allowed" : "pointer",
                   }}
-                  // onClick={clickHandler}
+                  onClick={clickHandler}
                   key={choice.description}
                 >
                   {selected && (
@@ -89,7 +86,7 @@ const CustomizeProducts = ({
                     boxShadow: disabled ? "none" : "",
                   }}
                   key={choice.description}
-                  // onClick={clickHandler}
+                  onClick={clickHandler}
                 >
                   {choice.description}
                 </li>
@@ -98,8 +95,15 @@ const CustomizeProducts = ({
           </ul>
         </div>
       ))}
+      {/* <Add
+        productId={productId}
+        variantId={
+          selectedVariant?._id || "00000000-0000-0000-0000-000000000000"
+        }
+        stockNumber={selectedVariant?.stock?.quantity || 0}
+      /> */}
       {/* COLOR */}
-      {/* <h4 className="font-medium">Choose a {option.name}</h4>
+      {/* 
           <ul className="flex items-center gap-3">
             <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500">
               <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
@@ -112,13 +116,13 @@ const CustomizeProducts = ({
       {/* OTHERS */}
       {/* <h4 className="font-medium">Choose a size</h4>
       <ul className="flex items-center gap-3">
-        <li className="ring-1 ring-ale text-ale rounded-md py-1 px-4 text-sm cursor-pointer">
+        <li className="ring-1 ring-lama text-lama rounded-md py-1 px-4 text-sm cursor-pointer">
           Small
         </li>
-        <li className="ring-1 ring-ale text-white bg-ale rounded-md py-1 px-4 text-sm cursor-pointer">
+        <li className="ring-1 ring-lama text-white bg-lama rounded-md py-1 px-4 text-sm cursor-pointer">
           Medium
         </li>
-        <li className="ring-1 ring-pink-200 bg-pink-200 text-white rounded-md py-1 px-4 text-sm cursor-not-allowed">
+        <li className="ring-1 ring-pink-200 text-white bg-pink-200 rounded-md py-1 px-4 text-sm cursor-not-allowed">
           Large
         </li>
       </ul> */}
