daf2c21fbb9c6320307d923d54d9fe27aae5ecfb
Alek30k <alemank23k@gmail.com>
2024-06-06 22:51:14 -0300

    last commit

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 const Pagination = ({
   currentPage,
   hasPrev,
@@ -9,12 +11,28 @@ const Pagination = ({
   hasPrev: boolean;
   hasNext: boolean;
 }) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+
+  const createPageUrl = (pageNumber: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", pageNumber.toString());
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <div className="mt-12 flex justify-between w-full">
-      <button className="rounded-md bg-ale text-white p-2 text-sm w-24 cursor-pointer disabled:cursor-not-allowed disabled:bg-pink-200">
+      <button
+        className="rounded-md bg-ale text-white p-2 text-sm w-24 cursor-pointer disabled:cursor-not-allowed disabled:bg-pink-200"
+        disabled={!hasPrev}
+      >
         Previous
       </button>
-      <button className="rounded-md bg-ale text-white p-2 text-sm w-24 cursor-pointer disabled:cursor-not-allowed disabled:bg-pink-200">
+      <button
+        className="rounded-md bg-ale text-white p-2 text-sm w-24 cursor-pointer disabled:cursor-not-allowed disabled:bg-pink-200"
+        disabled={!hasNext}
+      >
         Next
       </button>
     </div>
