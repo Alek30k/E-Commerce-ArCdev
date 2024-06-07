cc61b2341a62714be73d4f7f8232ce276fc5a971
Alek30k <alemank23k@gmail.com>
2024-06-07 16:20:46 -0300

    last commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useWixClient } from "@/Hooks/useWixClient";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 enum MODE {
   LOGIN = "LOGIN",
@@ -10,6 +12,9 @@ enum MODE {
 }
 
 const LoginPage = () => {
+  const wixClient = useWixClient();
+  const router = useRouter();
+
   const [mode, setMode] = useState(MODE.LOGIN);
 
   const [username, setUsername] = useState("");
@@ -20,7 +25,88 @@ const LoginPage = () => {
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
 
-  return <div className="">LoginPage</div>;
+  const formTitle =
+    mode === MODE.LOGIN
+      ? "Log in"
+      : mode === MODE.REGISTER
+      ? "Register"
+      : mode === MODE.RESET_PASSWORD
+      ? "Reset Your Password"
+      : "Verify Your Email";
+
+  const buttonTitle =
+    mode === MODE.LOGIN
+      ? "Login"
+      : mode === MODE.REGISTER
+      ? "Register"
+      : mode === MODE.RESET_PASSWORD
+      ? "Reset"
+      : "Verify";
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="h-[calc(100vh-80px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center">
+      <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+        <h1 className="text-2xl font-semibold">{formTitle}</h1>
+        {mode === MODE.REGISTER ? (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="john"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+        ) : null}
+        {mode !== MODE.EMAIL_VERIFICATION ? (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">E-mail</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="john@gmail.com"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Verification Code</label>
+            <input
+              type="text"
+              name="emailCode"
+              placeholder="Code"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+              onChange={(e) => setEmailCode(e.target.value)}
+            />
+          </div>
+        )}
+        {mode === MODE.LOGIN || mode === MODE.REGISTER ? (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        ) : null}
+        {mode === MODE.LOGIN && (
+          <div
+            className="text-sm underline cursor-pointer"
+            onClick={() => setMode(MODE.RESET_PASSWORD)}
+          >
+            Forgot Password?
+          </div>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
