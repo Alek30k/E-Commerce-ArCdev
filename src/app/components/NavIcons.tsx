f36738f6054b6415003dbb12fd4fb08acc9af99e
Alek30k <alemank23k@gmail.com>
2024-06-13 10:29:50 -0300

    last commit

--- src/app/components/NavIcons.tsx
+++ src/app/components/NavIcons.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import CartModal from "./CartModal";
-import { useWixClient } from "@/Hooks/useWixClient";
 import Cookies from "js-cookie";
+import { useWixClient } from "@/Hooks/useWixClient";
 
 const NavIcons = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
@@ -14,9 +14,13 @@ const NavIcons = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
+  // const pathName = usePathname();
 
   const wixClient = useWixClient();
   const isLoggedIn = wixClient.auth.loggedIn();
+
+  // TEMPORARY
+  // const isLoggedIn = false;
 
   const handleProfile = () => {
     if (!isLoggedIn) {
@@ -42,15 +46,6 @@ const NavIcons = () => {
   //   window.location.href = authUrl;
   // };
 
-  // const handleLogout = async () => {
-  //   setIsLoading(true);
-  //   Cookies.remove("refreshToken");
-  //   const { logoutUrl } = await wixClient.auth.logout(window.location.href);
-  //   setIsLoading(false);
-  //   setIsProfileOpen(false);
-  //   router.push(logoutUrl);
-  // };
-
   const handleLogout = async () => {
     setIsLoading(true);
     Cookies.remove("refreshToken");
@@ -60,26 +55,32 @@ const NavIcons = () => {
     router.push(logoutUrl);
   };
 
+  // useEffect(() => {
+  //   getCart(wixClient);
+  // }, [wixClient, getCart]);
+
   return (
     <div className="flex items-center gap-4 xl:gap-6 relative">
       <Image
         src="/profile.png"
-        alt="profile"
+        alt=""
         width={22}
         height={22}
         className="cursor-pointer"
-        onClick={handleProfile}
         // onClick={login}
+        onClick={handleProfile}
       />
       {isProfileOpen && (
-        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
-          <Link href="/">Profile</Link>
-          <div className="mt-2 cursor-pointer">Logout</div>
+        <div className="absolute p-4 rounded-md top-12 left-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+          <Link href="/profile">Profile</Link>
+          <div className="mt-2 cursor-pointer" onClick={handleLogout}>
+            {isLoading ? "Logging out" : "Logout"}
+          </div>
         </div>
       )}
       <Image
         src="/notification.png"
-        alt="notification"
+        alt=""
         width={22}
         height={22}
         className="cursor-pointer"
@@ -88,13 +89,7 @@ const NavIcons = () => {
         className="relative cursor-pointer"
         onClick={() => setIsCartOpen((prev) => !prev)}
       >
-        <Image
-          src="/cart.png"
-          alt="cart"
-          width={22}
-          height={22}
-          className="cursor-pointer"
-        />
+        <Image src="/cart.png" alt="" width={22} height={22} />
         <div className="absolute -top-4 -right-4 w-6 h-6 bg-ale rounded-full text-white text-sm flex items-center justify-center">
           2
         </div>
