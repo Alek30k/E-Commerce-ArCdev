afba35e12305e3e5f937bdbcef5b2285bacae672
Alek30k <alemank23k@gmail.com>
2024-06-14 22:09:28 -0300

    last commit

--- src/app/components/CartModal.tsx
+++ src/app/components/CartModal.tsx
@@ -3,18 +3,25 @@
 import { useCartStore } from "@/Hooks/useCartStore";
 import { useWixClient } from "@/Hooks/useWixClient";
 import Image from "next/image";
+import { useEffect } from "react";
 
 const CartModal = () => {
   // TEMPORARY
-  const cartItems = true;
+  // const cartItems = true;
 
   const wixClient = useWixClient();
   // const { cart, isLoading, removeItem } = useCartStore();
   const { cart, getCart } = useCartStore();
 
+  useEffect(() => {
+    getCart(wixClient);
+  }, [wixClient, getCart]);
+
+  console.log(cart);
+
   return (
     <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
-      {!cartItems ? (
+      {!cart.lineItems ? (
         <div className="">Cart is Empty</div>
       ) : (
         <>
@@ -22,33 +29,41 @@ const CartModal = () => {
           {/* LIST */}
           <div className="flex flex-col gap-8">
             {/* ITEM */}
-            <div className="flex gap-4">
-              <Image
-                src="https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&w=600"
-                alt="cartImage"
-                width={72}
-                height={96}
-                className="object-cover rounded-md"
-              />
-              <div className="flex flex-col justify-between w-full">
-                {/* TOP */}
-                <div className="">
-                  {/* TITLE */}
-                  <div className="flex items-center justify-between gap-8">
-                    <h3 className="font-semibold">Product Name</h3>
-                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+            {cart.lineItems.map((item) => (
+              <div className="flex gap-4" key={item._id}>
+                <Image
+                  src="https://images.pexels.com/photos/3819969/pexels-photo-3819969.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="cartImage"
+                  width={72}
+                  height={96}
+                  className="object-cover rounded-md"
+                />
+                <div className="flex flex-col justify-between w-full">
+                  {/* TOP */}
+                  <div className="">
+                    {/* TITLE */}
+                    <div className="flex items-center justify-between gap-8">
+                      <h3 className="font-semibold">
+                        {item.productName?.original}
+                      </h3>
+                      <div className="p-1 bg-gray-50 rounded-sm">
+                        ${item.price?.amount}
+                      </div>
+                    </div>
+                    {/* DESC */}
+                    <div className="text-sm text-gray-500">
+                      {item.availability?.status}
+                    </div>
                   </div>
-                  {/* DESC */}
-                  <div className="text-sm text-gray-500">available</div>
-                </div>
 
-                {/* BOTTOM */}
-                <div className="flex justify-between text-sm">
-                  <span className="text-gray-500">Qty. 2</span>
-                  <span className="text-blue-500 cursor-pointer">Remove</span>
+                  {/* BOTTOM */}
+                  <div className="flex justify-between text-sm">
+                    <span className="text-gray-500">Qty. {item.quantity}</span>
+                    <span className="text-blue-500 cursor-pointer">Remove</span>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
           {/* BOTTOM */}
           <div className="">
