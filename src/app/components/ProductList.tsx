f3b83d3d6cb482fd80e589ee2f668ef2e4cfe573
Alek30k <alemank23k@gmail.com>
2024-06-22 14:44:02 -0300

    last commit

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -24,30 +24,30 @@ const ProductList = async ({
     .eq("collectionIds", categoryId)
     .hasSome(
       "productType",
-      searchParams?.type ? [searchParams?.type] : ["physical", "digital"]
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
     )
     .gt("priceData.price", searchParams?.min || 0)
     .lt("priceData.price", searchParams?.max || 999999)
     .limit(limit || PRODUCT_PER_PAGE)
     .skip(
       searchParams?.page
-        ? parseInt(searchParams?.page) * (limit || PRODUCT_PER_PAGE)
+        ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
         : 0
     );
   // .find();
 
   if (searchParams?.sort) {
-    const [sortType, sortBy] = searchParams?.sort.split(" ");
+    const [sortType, sortBy] = searchParams.sort.split(" ");
 
     if (sortType === "asc") {
-      productQuery?.ascending(sortBy);
+      productQuery.ascending(sortBy);
     }
     if (sortType === "desc") {
-      productQuery?.descending(sortBy);
+      productQuery.descending(sortBy);
     }
   }
 
-  const res = await productQuery?.find();
+  const res = await productQuery.find();
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
@@ -91,7 +91,7 @@ const ProductList = async ({
               }}
             ></div>
           )}
-          <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          <button className="rounded-2xl ring-1 ring-ale text-ale w-max py-2 px-4 text-xs hover:bg-ale hover:text-white">
             Add to Cart
           </button>
         </Link>
