1ec19bd16be1bb8d89e5d3ca2880f6dc45ddb973
Alek30k <alemank23k@gmail.com>
2024-06-24 12:01:00 -0300

    last commit

--- src/app/components/CartModal.tsx
+++ src/app/components/CartModal.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import { media as wixMedia } from "@wix/sdk";
-import { currentCart } from "@wix/ecom";
 import { useWixClient } from "@/Hooks/useWixClient";
 import { useCartStore } from "@/Hooks/useCartStore";
 
@@ -12,30 +11,6 @@ const CartModal = () => {
 
   const wixClient = useWixClient();
   const { cart, isLoading, removeItem } = useCartStore();
-
-  const handleCheckout = async () => {
-    try {
-      const checkout =
-        await wixClient.currentCart.createCheckoutFromCurrentCart({
-          channelType: currentCart.ChannelType.WEB,
-        });
-
-      const { redirectSession } =
-        await wixClient.redirects.createRedirectSession({
-          ecomCheckout: { checkoutId: checkout.checkoutId },
-          callbacks: {
-            postFlowUrl: window.location.origin,
-            thankYouPageUrl: `${window.location.origin}/success`,
-          },
-        });
-
-      if (redirectSession?.fullUrl) {
-        window.location.href = redirectSession.fullUrl;
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return (
     <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20 ">
@@ -118,7 +93,7 @@ const CartModal = () => {
               <button
                 className="rounded-md py-3 px-4 bg-black text-white disabled:cursor-not-allowed disabled:opacity-75"
                 disabled={isLoading}
-                onClick={handleCheckout}
+                // onClick={handleCheckout}
               >
                 Checkout
               </button>
