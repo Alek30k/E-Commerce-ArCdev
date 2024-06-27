3725c8104f9fd8286a39ca7ed90e6dd34700a778
Alek30k <alemank23k@gmail.com>
2024-06-26 22:27:42 -0300

    last commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -27,40 +27,44 @@ const ProfilePage = async () => {
       <div className="w-full md:w-1/2">
         <h1 className="text-2xl">Profile</h1>
         <form className="mt-12 flex flex-col gap-4">
-          <input type="text" hidden name="id" />
+          <input type="text" hidden name="id" value={user.member.contactId} />
           <label className="text-sm text-gray-700">Username</label>
           <input
             type="text"
             name="username"
-            placeholder="john"
+            placeholder={user.member?.profile?.nickname || "john"}
             className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
           />
           <label className="text-sm text-gray-700">First Name</label>
           <input
             type="text"
             name="firstName"
-            placeholder="John"
+            placeholder={user.member?.contact?.firstName || "John"}
             className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
           />
           <label className="text-sm text-gray-700">Surname</label>
           <input
             type="text"
             name="lastName"
-            placeholder="Doe"
+            placeholder={user.member?.contact?.lastName || "Doe"}
             className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
           />
           <label className="text-sm text-gray-700">Phone</label>
           <input
             type="text"
             name="phone"
-            placeholder="+1234567"
+            placeholder={
+              (user.member?.contact?.phones &&
+                user.member?.contact?.phones[0]) ||
+              "+1234567"
+            }
             className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
           />
           <label className="text-sm text-gray-700">E-mail</label>
           <input
             type="email"
             name="email"
-            placeholder="john@gmail.com"
+            placeholder={user.member?.loginEmail || "john@gmail.com"}
             className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
           />
           {/* <UpdateButton /> */}
