ebaae95bd329bdf431f6234b093fd9e33c2a1104
Alek30k <alemank23k@gmail.com>
2024-06-28 22:51:21 -0300

    last commit

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -6,13 +6,16 @@ export const updateUser = async (formData: FormData) => {
   const wixClient = await wixClientServer();
 
   const id = formData.get("id") as string;
+  if (!id || id.trim() === "") {
+    // Handle missing or empty ID
+    console.error("Missing or empty user ID");
+    return; // Or redirect to login or error page
+  }
   const username = formData.get("username") as string;
   const firstName = formData.get("firstName") as string;
   const lastName = formData.get("lastName") as string;
   const email = formData.get("email") as string;
   const phone = formData.get("phone") as string;
-
-  //   console.log(username);
 
   try {
     const response = await wixClient.members.updateMember(id, {
@@ -25,7 +28,7 @@ export const updateUser = async (formData: FormData) => {
       profile: { nickname: username || undefined },
     });
 
-    console.log(response);
+    // console.log(response);
   } catch (err) {
     console.log(err);
   }
