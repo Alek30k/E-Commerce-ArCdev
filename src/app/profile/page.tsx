429d1993b689d89939da918a4b22fb010e5627b6
Alek30k <alemank23k@gmail.com>
2024-06-29 10:59:28 -0300

    last commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,5 @@
 import { wixClientServer } from "@/lib/wixClientServer";
-// import { members } from "@wix/members";
+import { members } from "@wix/members";
 import Link from "next/link";
 import { format } from "timeago.js";
 // import UpdateButton from "../components/UpdateButton";
@@ -8,19 +8,19 @@ import { format } from "timeago.js";
 const ProfilePage = async () => {
   const wixClient = await wixClientServer();
 
-  // const user = await wixClient.members.getCurrentMember({
-  //   fieldsets: [members.Set.FULL],
-  // });
+  const user = await wixClient.members.getCurrentMember({
+    fieldsets: [members.Set.FULL],
+  });
 
-  // if (!user.member?.contactId) {
-  //   return <div className="">Not logged in!</div>;
-  // }
+  if (!user.member?.contactId) {
+    return <div className="">Not logged in!</div>;
+  }
 
-  // const orderRes = await wixClient.orders.searchOrders({
-  //   search: {
-  //     filter: { "buyerInfo.contactId": { $eq: user.member?.contactId } },
-  //   },
-  // });
+  const orderRes = await wixClient.orders.searchOrders({
+    search: {
+      filter: { "buyerInfo.contactId": { $eq: user?.member?.contactId } },
+    },
+  });
 
   return (
     <div className="flex flex-col md:flex-row gap-24 md:h-[calc(100vh-180px)] items-center px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
@@ -73,7 +73,7 @@ const ProfilePage = async () => {
       <div className="w-full md:w-1/2">
         <h1 className="text-2xl">Orders</h1>
         <div className="mt-12 flex flex-col">
-          {/* {orderRes?.orders?.map((order) => (
+          {orderRes?.orders?.map((order) => (
             <Link
               href={`/orders/${order._id}`}
               key={order._id}
@@ -88,7 +88,7 @@ const ProfilePage = async () => {
               )}
               <span className="w-1/4">{order.status}</span>
             </Link>
-          ))} */}
+          ))}
         </div>
       </div>
     </div>
