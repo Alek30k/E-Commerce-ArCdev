81806de3fcb4fee4d5ff56a4a30eef4b7ec28916
Alek30k <alemank23k@gmail.com>
2024-07-01 12:25:54 -0300

    last commit

--- src/lib/wixClientServer.ts
+++ src/lib/wixClientServer.ts
@@ -1,21 +1,20 @@
 import { OAuthStrategy, createClient } from "@wix/sdk";
 import { collections, products } from "@wix/stores";
 import { orders } from "@wix/ecom";
-// import { cookies } from "next/headers";
+import { cookies } from "next/headers";
 import { members } from "@wix/members";
 
 export const wixClientServer = async () => {
-  // let refreshToken = null;
-
-  // try {
-  //   const cookieStore = cookies();
-  //   const refreshTokenCookie = cookieStore.get("refreshToken");
-  //   if (refreshTokenCookie) {
-  //     refreshToken = JSON.parse(refreshTokenCookie.value || "null");
-  //   }
-  // } catch (error) {
-  //   console.error("Error al leer la cookie refreshToken:", error);
-  // }
+  let refreshToken = null;
+  try {
+    const cookieStore = cookies();
+    const refreshTokenCookie = cookieStore.get("refreshToken");
+    if (refreshTokenCookie) {
+      refreshToken = JSON.parse(refreshTokenCookie.value || "null");
+    }
+  } catch (error) {
+    console.error("Error al leer la cookie refreshToken:", error);
+  }
 
   const wixClient = createClient({
     modules: {
@@ -25,11 +24,11 @@ export const wixClientServer = async () => {
       members,
     },
     auth: OAuthStrategy({
-      clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
-      // tokens: {
-      //   refreshToken,
-      //   accessToken: { value: "", expiresAt: 0 },
-      // },
+      clientId: "4d78749b-506c-4c11-bf41-27e528e4f58a",
+      tokens: {
+        refreshToken,
+        accessToken: { value: "", expiresAt: 0 },
+      },
     }),
   });
 
